aac2b97ff039aeb0f5d8141ccbefd33ef0bbbb6d
Sam Plackett <samuel.plackett@eviden.com>
2026-09-24 16:51:33 +0100

    reduce complexity

--- app/auth/map-auth.js
+++ app/auth/map-auth.js
@@ -16,22 +16,25 @@ const {
 } = require('./permissions')
 
 const mapAuth = (request) => {
+  const { isAuthenticated, credentials } = request.auth
+  const hasRole = role => isAuthenticated && isInRole(credentials, role)
+
   return {
-    isAuthenticated: request.auth.isAuthenticated,
-    isAnonymous: !request.auth.isAuthenticated,
-    isApplicationAdmin: request.auth.isAuthenticated && isInRole(request.auth.credentials, applicationAdmin),
-    isReportViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, reportViewer),
-    isEventViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, eventViewer),
-    isHoldViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, holdViewer),
-    isInjectionViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, injectionViewer),
-    isClosureViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, closureViewer),
-    isAlertViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, alertViewer),
-    isStatementViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, statementViewer),
-    isMetricsViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, metricsViewer),
-    isResetViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, resetViewer),
-    isSequenceViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, sequenceViewer),
-    isDebtDataViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, debtDataViewer),
-    isLedgerViewerUser: request.auth.isAuthenticated && isInRole(request.auth.credentials, ledgerViewer)
+    isAuthenticated,
+    isAnonymous: !isAuthenticated,
+    isApplicationAdmin: hasRole(applicationAdmin),
+    isReportViewerUser: hasRole(reportViewer),
+    isEventViewerUser: hasRole(eventViewer),
+    isHoldViewerUser: hasRole(holdViewer),
+    isInjectionViewerUser: hasRole(injectionViewer),
+    isClosureViewerUser: hasRole(closureViewer),
+    isAlertViewerUser: hasRole(alertViewer),
+    isStatementViewerUser: hasRole(statementViewer),
+    isMetricsViewerUser: hasRole(metricsViewer),
+    isResetViewerUser: hasRole(resetViewer),
+    isSequenceViewerUser: hasRole(sequenceViewer),
+    isDebtDataViewerUser: hasRole(debtDataViewer),
+    isLedgerViewerUser: hasRole(ledgerViewer)
   }
 }
 
